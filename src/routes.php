ca8edc7a16a18bfc86f0344ac7d7021cf3be0903
namir abboud <namir.abboud@tedmob.com>
2020-11-20 14:28:23 +0200

    update

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,13 @@
 
 Route::group(['middleware' => ['web','auth:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function() {
 
-    Route::resource('general', 'Eve\Cms\Controllers\GeneralController');
+    Route::resource('general', 'Eve\Dynamic\Controllers\GeneralController');
 
     //multiple images
-    Route::post('general-upload-image', 'Eve\Cms\Controllers\GeneralController@imageUpload')->name('generalImage.upload');
-    Route::post('general-delete-image', 'Eve\Cms\Controllers\GeneralController@deleteUpload')->name('generalImage.delete');
-    Route::post('edit-general-delete-image', 'Eve\Cms\Controllers\GeneralController@deleteUploadEdit')->name('generalImage.deleteEdit');
-    Route::get('general-images/{id}', 'Eve\Cms\Controllers\GeneralController@getDefault')->name('generalImage.get');
+    Route::post('general-upload-image', 'Eve\Dynamic\Controllers\GeneralController@imageUpload')->name('generalImage.upload');
+    Route::post('general-delete-image', 'Eve\Dynamic\Controllers\GeneralController@deleteUpload')->name('generalImage.delete');
+    Route::post('edit-general-delete-image', 'Eve\Dynamic\Controllers\GeneralController@deleteUploadEdit')->name('generalImage.deleteEdit');
+    Route::get('general-images/{id}', 'Eve\Dynamic\Controllers\GeneralController@getDefault')->name('generalImage.get');
 });
 
 
